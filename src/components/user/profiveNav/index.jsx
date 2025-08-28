38758d6c11cfe536076af669f6e39f2eddb4dba8
Fuel It Online <fuelitonline@gmail.com>
2025-08-28 17:46:37 +0530

    Update index.jsx

--- src/components/user/profiveNav/index.jsx
+++ src/components/user/profiveNav/index.jsx
@@ -187,7 +187,7 @@ function ProfileNav() {
       >
         <MenuItem
           component={Link}
-          to= {user?.role === 'superAdmin' ? "https://admin.immultiverse.co" : "https://user.immultiverse.co"}
+          to= "/profile"
           onClick={handleMenuClose}
           sx={{
             py: 1.5,
